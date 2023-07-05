21dccfa659e00a2646b7dc543c834ae59bc4e708
Pavit Pimchanagul <pavitpim@gmail.com>
2023-07-06 01:46:01 +0700

    controller getTodoById

--- __tests__/units/todo.controller.test.js
+++ __tests__/units/todo.controller.test.js
@@ -71,3 +71,61 @@ describe('TodoController.createTodo', () => {
         expect(next).toBeCalledWith(errorMessage);
     });
 });
+
+describe('TodoController.getTodoById', () => {
+    it('should have a getTodoById', () => {
+        expect(typeof TodoController.getTodoById).toBe('function');
+    });
+
+    it('should call TodoModel.findByPk', async () => {
+        // Arrange
+        req.params.todoId = 1;
+        Todo.findByPk.mockReturnValue(newTodo);
+
+        // Act
+        await TodoController.getTodoById(req, res, next);
+
+        // Assert
+        expect(Todo.findByPk).toBeCalledWith(req.params.todoId);
+        expect(Todo.findByPk).toBeCalledTimes(1);
+    });
+
+    it('should return json body and response code 200', async () => {
+        // Arrange
+        req.params.todoId = 1;
+        Todo.findByPk.mockReturnValue(newTodo);
+
+        // Act
+        await TodoController.getTodoById(req, res, next);
+
+        // Assert
+        expect(res.statusCode).toBe(200);
+        expect(res._getJSONData()).toStrictEqual(newTodo);
+        expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    it('should return 404 when item does not exist', async () => {
+        // Arrange
+        Todo.findByPk.mockReturnValue(null);
+
+        // Act
+        await TodoController.getTodoById(req, res, next);
+
+        // Assert
+        expect(res.statusCode).toBe(404);
+        expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    it('should handle errors', async () => {
+        // Arrange
+        const errorMessage = { message: 'error finding todoModel' };
+        const rejectedPromise = Promise.reject(errorMessage);
+        Todo.findByPk.mockReturnValue(rejectedPromise);
+
+        // Act
+        await TodoController.getTodoById(req, res, next);
+
+        // Assert
+        expect(next).toHaveBeenCalledWith(errorMessage);
+    });
+});
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -12,3 +12,19 @@ exports.createTodo = async (req, res, next) => {
         next(err);
     }
 };
+
+exports.getTodoById = async (req, res, next) => {
+    try {
+        const todoId = req.params.todoId;
+
+        const newTodo = await TodoService.getTodoById(todoId);
+
+        if (!newTodo) {
+            return res.status(404).send();
+        }
+
+        return res.status(200).json(newTodo);
+    } catch (err) {
+        next(err);
+    }
+};
